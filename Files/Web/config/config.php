c4f0344e5a10fcecca95bba1a2fd3a481e99de15
Nathan Istace <nathan.istace@gmail.com>
2021-10-09 14:14:57 +0200

    Delete Files/Web/config directory

--- Files/Web/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("__ENV", "test");
-define("__URI_ROOT", "/ld49/");
-
-define("DB_HOST", "localhost");
-define("DB_PORT", "");
-define("DB_NAME", "nathanistace");
-define("DB_USER", "root");
-define("DB_PASS", "");
--- Files/Web/config/config_prod.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("__URI_ROOT", "https://tddg.nathanistace.be");
-define("__ENV", "prod");
-
-define("DB_HOST", "nathanisaudb.mysql.db");
-define("DB_PORT", "");
-define("DB_NAME", "nathanisaudb");
-define("DB_USER", "nathanisaudb");
-define("DB_PASS", "11TQCtHrLCvABYam5IWe");
